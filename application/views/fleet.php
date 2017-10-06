fb3de5da33c7e13bdf393c0b9de9b2ae745711b5
Michael Chen <mc70050@gmail.com>
2017-10-06 00:18:23 -0700

    Fleet controller, view, model in working condition

---
+++ application/views/fleet.php
@@ -0,0 +1,16 @@
+<div id="body">
+    
+    <table>
+        <tr>
+            <td>Airplane ID</td>
+            <td>Type</td>
+        </tr>
+        {planes}
+        <tr>
+            <td>{id}</td>
+            <td>{type}</td>
+        </tr>
+        {/planes}
+    </table>
+    
+</div>
