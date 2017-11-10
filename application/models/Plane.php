c8177a4b4d30938d17d7ccc635cbf78d6d8c9266
Daniel <daniel51_1994@hotmail.com>
2017-11-10 08:36:27 -0800

    Update Flight getters and setters

--- application/models/Plane.php
+++ application/models/Plane.php
@@ -6,7 +6,14 @@
  */
 class Plane extends Entity {
     private $id;
-    private $type;
+    private $manufacturer;
+    private $model;
+    private $price;
+    private $seats;
+    private $reach;
+    private $cruise;
+    private $takeoff;
+    private $hourly;
 
     // Set the id of the plane
     public function setId($id) {
@@ -14,8 +21,43 @@ class Plane extends Entity {
     }
 
     // Set the plane type
-    public function setType($type) {
-        $this->type = $type;
+    public function setManufacturer($manufacturer) {
+        $this->manufacturer = $manufacturer;
+    }
+
+    // Set the model of the plane
+    public function setModel($model) {
+        $this->model = $model;
+    }
+
+    // Set the price of the plane
+    public function setPrice($price) {
+        $this->price = $price;
+    }
+
+    // Set the number of seats
+    public function setSeats($seats) {
+        $this->seats = $seats;
+    }
+
+    // Set the reach number
+    public function setReach($reach) {
+        $this->reach = $reach;
+    }
+
+    // Set the cruise
+    public function setCruise($cruise) {
+        $this->cruise = $cruise;
+    }
+
+    // Set the takeoff
+    public function setTakeoff($takeoff) {
+        $this->takeoff = $takeoff;
+    }
+
+    // Set the hour
+    public function setHourly($hourly) {
+        $this->hourly = $hourly;
     }
 
     // Return plane ID
@@ -24,7 +66,38 @@ class Plane extends Entity {
     }
 
     // Return plane type
-    public function getType() {
-        return $this->type;
+    public function getManufacturer() {
+        return $this->manufacturer;
+    }
+
+    // Return plane model
+    public function getModel() {
+        return $this->model;
+    }
+
+    // Return the price of the plane
+    public function getPrice() {
+        return $this->price;
+    }
+
+    // Return the number of seats
+    public function getSeats() {
+        return $this->seats;
+    }
+
+    public function getReach() {
+        return $this->reach;
+    }
+
+    public function getCruise() {
+        return $this->cruise;
+    }
+
+    public function getTakeoff() {
+        return $this->takeoff;
+    }
+
+    public function getHourly() {
+        return $this->hourly;
     }
 }
