23d564e78939c403ad32bb45c073c8b7a96b4cd7
Daniel <daniel51_1994@hotmail.com>
2017-11-09 18:33:49 -0800

    Create Flight entity

--- application/models/Flight.php
+++ application/models/Flight.php
@@ -1,12 +1,44 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: danie
- * Date: 2017-11-10
- * Time: 1:24 AM
+ * Class Flight
+ *
+ * @author Daniel Park
  */
+class Flight extends Entity {
+    private $id;
+    private $departure;
+    private $arrival;
+    private $departureTime;
+    private $arrivalTime;
 
-class Flight
-{
+    // id = unique alphanumeric identifier (starting with first letter of team name)
+    // departure = no departure before 08:00
+    // arrival = no landing after 22:00
+    // minimum 30 min between plane's landing and departure
+    // fleet must be back by the end of the day
 
+    // Set the ID of the flight
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    // Set the departure airport of the flight
+    public function setDeparture($name) {
+        $this->departure = $name;
+    }
+
+    // Set the arrival airport of the flight
+    public function setArrival($name) {
+        $this->arrival = $name;
+    }
+
+    // Set the departure time of the flight
+    public function setDepartureTime($time) {
+        $this->departureTime = $time;
+    }
+
+    // Set the arrival time of the flight
+    public function setArrivalTime($time) {
+        $this->arrivalTime = $time;
+    }
 }
