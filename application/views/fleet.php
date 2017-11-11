88bf2b6ab0dc6ba6ae627e12b1c97725dd831b9a
Daniel <daniel51_1994@hotmail.com>
2017-11-10 23:57:11 -0800

    Update views

--- application/views/fleet.php
+++ application/views/fleet.php
@@ -3,13 +3,27 @@
     
     <table>
         <tr>
-            <td>Airplane ID</td>
-            <td>Type</td>
+            <td>Fleet ID</td>
+            <td>Manufacturer</td>
+            <td>Model</td>
+            <td>Price</td>
+            <td>Seats</td>
+            <td>Reach</td>
+            <td>Cruise</td>
+            <td>Takeoff</td>
+            <td>Hourly</td>
         </tr>
         {planes}
         <tr>
             <td>{id}</td>
-            <td>{type}</td>
+            <td>{manufacturer}</td>
+            <td>{model}</td>
+            <td>{price}</td>
+            <td>{seats}</td>
+            <td>{reach}</td>
+            <td>{cruise}</td>
+            <td>{takeoff}</td>
+            <td>{hourly}</td>
         </tr>
         {/planes}
     </table>
