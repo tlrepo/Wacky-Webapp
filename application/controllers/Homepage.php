0a15eef0eeeab5e49e0f212adb2a88e9f3e550f2
Daniel <daniel51_1994@hotmail.com>
2017-11-10 23:53:26 -0800

    Comment out flight count (temporary)

--- application/controllers/Homepage.php
+++ application/controllers/Homepage.php
@@ -19,17 +19,17 @@ class Homepage extends Application {
         $flightData = $this->flights_model->all();
         $planesSource = $this->planes->all();
         $destination = array();
-        foreach($flightData as $flights) {
+        /*foreach($flightData as $flights) {
             if(!in_array($flights['airline'], $destination)) {
                 array_push($destination, $flights['airline']);
             }
-        }
+        }*/
         //Split the array up with commas
         $role = $this->session->userdata('userrole');    
         $this->data['pagetitle'] = 'Homepage ('. $role . ')';
         $this->data['destinationAirline'] = implode(', ', $destination);
         $this->data['numOfPlanes'] = count($planesSource);
-        $this->data['numOfFlights'] = count($flightData);
+        //$this->data['numOfFlights'] = count($flightData);
         $this->data['pagebody'] = 'homepage';
         $this->data['income'] = '$16,777,216';
         $this->render();
